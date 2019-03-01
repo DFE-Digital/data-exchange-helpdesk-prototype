0041b9af5fe0bdfe6f11e2ef526b512c4f699c34
North East Digital Ltd <ne-digital@David-Todds-MacBook-Pro.local>
2019-03-01 09:11:30 +0000

    Added work queue logic

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -17,10 +17,233 @@ Example usage:
 
 */
 
+const faker = require('faker')
+
+const randomFirstName = faker.name.firstName
+
+const randomLastName = faker.name.lastName
+
+const randomName = _ => {
+	return randomFirstName() + ' ' + randomLastName()
+}
+
+const randomItemFrom = array => {
+	return array[Math.floor(Math.random() * array.length)]
+}
+
+const createPupils = amount => {
+	const upnPrefixes = ['W', 'X', 'P']
+	const upnSuffixes = ['B', 'D', 'M']
+	const days = new Array(28).fill(null).map((_, i) => i + 1)
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	]
+	const years = new Array(8).fill(null).map((_, i) => i + 2009)
+	const upnNumber = length => {
+		const numberArray = new Array(length)
+			.fill(null)
+			.map(_ => Math.floor(Math.random() * 9).toString())
+		return numberArray.join('')
+	}
+	return new Array(amount).fill(null).map(_ => {
+		return {
+			surname: randomLastName().toUpperCase(),
+			firstname: randomFirstName(),
+			UPN:
+				randomItemFrom(upnPrefixes) +
+				upnNumber(10) +
+				randomItemFrom(upnSuffixes),
+			dob:
+				randomItemFrom(days) +
+				' ' +
+				randomItemFrom(months) +
+				' ' +
+				randomItemFrom(years)
+		}
+	})
+}
+
+const createCollectors = amount => {
+	return new Array(amount).fill(null).map(_ => {
+		return {
+			name: randomName(),
+			las: [],
+			weighting: randomItemFrom([1, 1, 1, 1, 0.75, 0.5])
+		}
+	})
+}
+
+const councils = [
+	'Barnfordshire Council',
+	'Barsetshire City',
+	'Borsetshire County Council',
+	'Burton City Council',
+	'Didminster County Council',
+	'Downshire City Council',
+	'Ffhagdiwedd City Council',
+	'Waliens County Council',
+	'Gaultshire City',
+	'Glebeshire Council',
+	'Glenshire City Council',
+	'Low Lissingbury Council',
+	'Loamshire City Council',
+	'Mangelwurzelshire Council',
+	'Markshire Council',
+	'Mallardshire Council',
+	'Melfordshire City Council',
+	'Mertonshire County Council',
+	'Mortshire City Council',
+	'Midsomer City Council',
+	'Mummerset County Council',
+	'Naptonshire City',
+	'Norbridgeshire Council',
+	'Oatshire City Council',
+	'Placefordshire Council',
+	'Quantumshire City Council',
+	'Radfordshire City Council',
+	'Downshire Council',
+	'Glenshire Council',
+	'Southshire County Council',
+	'Middleshire Council',
+	'Redshire City Council',
+	'Russetshire Council',
+	'Rutshire City City Council',
+	'Shiring County Council',
+	'Shroudshire Council',
+	'Slopshire County Council',
+	'Southmoltonshire',
+	'Stonyshire City Council',
+	'Trumlonshire Council',
+	'Trumbton Council',
+	'Chigley County Council',
+	'Wessex City Council',
+	'Westershire Council',
+	'Waringham Council',
+	'Westshire County Council',
+	'Winshire County Council',
+	'Wordenshire Council',
+	'Worfordshire Council',
+	'Wyvern County Council',
+	'Crow County Council',
+	'Mertshire Council',
+	'Calusa City Council',
+	'Livermore Council',
+	'Renard Council',
+	'Pontrain County Council',
+	'Belle Reve City Council',
+	'Ramilly Council',
+	'Herndon Council',
+	'Mist County Council',
+	'Blithe County Council',
+	'Ford Council',
+	'Huntington Council',
+	'Mayberry Council',
+	'Arlen Council',
+	'Belken County Council',
+	'Braddock County Council',
+	'Blackwood Council',
+	'Battenhill Council',
+	'Coldgrass Council',
+	'Dormarsh City',
+	'Deepston County Council',
+	'Fallfay City Council',
+	'Fairport County Council',
+	'Wateredge City Council',
+	'Moorhall City Council',
+	'Butterness County Council',
+	'Highburn City',
+	'Strongby Council',
+	'Marblepond City Council',
+	'Low Byfield Council',
+	'Bushmere City Council',
+	'Brightlake Council',
+	'Highfair Council',
+	'Stoneshade Council',
+	'Wintermallow City Council',
+	'Stonewell County Council',
+	'Rosesea City Council',
+	'Elfport City Council',
+	'Shadeborough County Council',
+	'Grasscliff City',
+	'Bridgecastle Council',
+	'Dracwick City Council',
+	'Pinebridge Council',
+	'Eastsage City Council',
+	'Waterton City Council',
+	'Wildeden Council',
+	'Lorfort Council',
+	'Whiteley County Council',
+	'Mistburn Council',
+	'Raymill City Council',
+	'Byley Council',
+	'Summerbrook City City Council',
+	'Aldlake County Council',
+	'Oldston Council',
+	'Erihall County Council',
+	'Erimarsh Council',
+	'Lochacre City Council',
+	'Freyhurst Council',
+	'Lochholt Council',
+	'Newston County Council',
+	'Cliffholt City Council',
+	'Belpond Council',
+	'Falledge Council',
+	'Applelake County Council',
+	'Woodedge County Council',
+	'Redvale Council',
+	'Verthollow Council',
+	'Lightmoor County Council',
+	'Swynhall County Council',
+	'Swynholt Council',
+	'Snowwheat City Council',
+	'Bayedge Council',
+	'Lochwind Council',
+	'Ornesse County Council',
+	'Morlea Well City Council',
+	'Wildeview Council',
+	'Waytown Council',
+	'Pryvale County Council',
+	'Northby County Council',
+	'Oakedge Council',
+	'Oldnesse Council',
+	'Wayland Council',
+	'Loredge Council',
+	'Normoor County Council',
+	'Aeledge County Council',
+	'Southlea Council',
+	'Marblemill Council'
+]
+
+const createLAs = (minSchools, maxSchools) => {
+	return new Array(councils.length()).fill(null).map((_, i) => {
+		return {
+			name: councils[i],
+			schoolCount: Math.floor(Math.random() * maxSchools) + minSchools
+		}
+	})
+}
+
 module.exports = {
 	// Insert values here
 
+	username: randomName(),
+
 	'selected-school': 0,
+
+	collectors: createCollectors(15),
+
+	localAuthorities: createLAs(1, 600),
 
 	schools: [
 		{
@@ -41,56 +264,25 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Nilda Roger',
+							author: randomName(),
 							text:
 								'The pupil is dually registered and did not attend school this term.',
 							date: '26 January 2019 at 9:43'
 						},
 						{
 							type: 'reply',
-							author: 'Shantell Heister',
+							author: randomName(),
 							text: 'It is not clear if this refers to all pupils.',
 							date: '26 January 2019 at 15:43'
 						},
 						{
 							type: 'school',
-							author: 'Lonnie Marbury',
+							author: randomName(),
 							text: 'Yes, this applies to all 3 pupils.',
 							date: '27 January 2019 at 8:23'
 						}
 					],
-					pupils: [
-						{
-							surname: 'CAIN',
-							firstname: 'Jaylahs',
-							UPN: 'W30433221895A',
-							dob: '23 August 2008'
-						},
-						{
-							surname: 'ARCHER',
-							firstname: 'Allyson',
-							UPN: 'W30433221932B',
-							dob: '29 August 2009'
-						},
-						{
-							surname: 'ZIMMERMAN',
-							firstname: 'Ella',
-							UPN: 'W30433221932B',
-							dob: '13 February 2008'
-						},
-						{
-							surname: 'GILBERT',
-							firstname: 'Andres',
-							UPN: 'W30433222132B',
-							dob: '2 April 2011'
-						},
-						{
-							surname: 'TRUJILLO',
-							firstname: 'Karissa',
-							UPN: 'W304332222312A',
-							dob: '4 June 2013'
-						}
-					]
+					pupils: createPupils(14)
 				},
 				{
 					ref: 1,
@@ -113,7 +305,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Elden Brown',
+							author: randomName(),
 							text:
 								'Pupil was added to this term by mistake, will be off-role on next census.',
 							date: '26 January 2019 at 9:45'
@@ -133,7 +325,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Velva Panton',
+							author: randomName(),
 							text: 'The school is in an area of high unemployment.',
 							date: '26 January 2019 at 9:51'
 						}
@@ -150,7 +342,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Lilian Mahi',
+							author: randomName(),
 							text: 'Early start date agreed by governors.',
 							date: '26 January 2019 at 10:03'
 						}
@@ -169,7 +361,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Alyce Belden',
+							author: randomName(),
 							text: 'A new school opened close by.',
 							date: '26 January 2019 at 11:00'
 						}
@@ -185,18 +377,11 @@ module.exports = {
 						'Pupil has an exemption for English GCSE funding due to learning difficulties but does not have  an education, health and care plan (EHCP).',
 					guide:
 						'As this impacts on funding a reason will be sought why a student who does not have an EHC plan would have a funding exemption which states the student has a learning disability which prevents them from studying the qualification',
-					pupils: [
-						{
-							surname: 'CAIN',
-							firstname: 'Jaylahs',
-							UPN: 'W30433221895A',
-							dob: '2 April 2011'
-						}
-					],
+					pupils: createPupils(1),
 					notes: [
 						{
 							type: 'school',
-							author: 'Mylo Deacon',
+							author: randomName(),
 							text:
 								'Special relief agreed by LA, authorised by Arnold Calderon.',
 							date: '26 January 2019 at 11:31'
@@ -216,7 +401,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Theresa Knight',
+							author: randomName(),
 							text: 'This is correct.',
 							date: '26 January 2019 at 11:22'
 						}
@@ -235,7 +420,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Theresa Knight',
+							author: randomName(),
 							text: 'We’re waiting on hearing back from the pupil’s parents.',
 							date: '26 January 2019 at 11:22'
 						}
@@ -256,7 +441,7 @@ module.exports = {
 					notes: [
 						{
 							type: 'school',
-							author: 'Theresa Knight',
+							author: randomName(),
 							text: 'Confirmed as correct',
 							date: '25 January 2019 at 11:22'
 						}
